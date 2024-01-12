e93374dc45d59ee56834ffb9083045334cff8ba3
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-01-12 15:02:30 +0100

    Add Menu class and icon for app.

--- constants.py
+++ constants.py
@@ -8,8 +8,38 @@
 @licence: GPLv3
 """
 import pygame as pg
-import pygame_menu as pgm
-import requests
-import json
-import os
 
+ABOUT = [f'POKEMON BATTLE CLONE',
+         f'Version: 0.1',
+         f'Bruno COULET',
+         f'Ugo D\'AMBROSIO',
+         f'Cyril GENISSON',
+         ]
+
+
+COLORS = {'BLACK': (0, 0, 0),
+          'WHITE': (255, 255, 255),
+          'RED': (255, 0, 0),
+          'GREEN': (0, 255, 0),
+          'BLUE': (0, 0, 255),
+          'YELLOW': (255, 255, 0),
+          'CYAN': (0, 255, 255),
+          'MAGENTA': (255, 0, 255)}
+
+#
+# DISPLAY
+#
+DSP_WIDTH = 800
+DSP_HEIGHT = 600
+FPS = 80
+SCREEN = pg.display.set_mode((DSP_WIDTH, DSP_HEIGHT), 0, 0, 0, 0)
+pg.display.set_caption("Pokemon Battle Clone")
+ico = pg.image.load('assets/images/Pokeball_icon.svg')
+pg.display.set_icon(ico)
+clock = pg.time.Clock()
+
+#
+# MENU
+#
+MENU_WIDTH = DSP_WIDTH * 1
+MENU_HEIGHT = DSP_HEIGHT * 1
