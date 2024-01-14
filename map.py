8efad78eb78929bd74efb9b2dab5f5e3690eecbf
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-01-14 20:58:48 +0100

    Battle class updated with news methods

--- map.py
+++ map.py
@@ -1,66 +1,39 @@
-from constants import *
-from main_test import * # pg.init() et run = True
+from constants import DSP_WIDTH, DSP_HEIGHT
+import random
+import pygame as pg
+
 
 class Map(pg.sprite.Sprite):
-
-    def __init__(self,x ,y):
-        # appel la super classe pour initialiser le sprite
+    def __init__(self):
         super().__init__()
-        # récupère le sprite sheet
+        self.img_size = 258, 146
+        self.img_width, self.img_height = self.img_size
         self.sprite_sheet = pg.image.load('assets/images/battle.png')
-        # Récupère l'image générée ci-dessous avec def get_image
-        self.image = self.get_image (0, 0)
-        # rectangle de Position du sprite
+        self.image = self.get_image(0, 0)
         self.rect = self.image.get_rect()
-        # différentes images du sprite 
         self.images = {
-            'map_1' : self.get_image(0,0),
-            'map_2' : self.get_image(259,0),
-            'map_3' : self.get_image(518,0),
-            'map_4' : self.get_image(774,0),
-            'map_5' : self.get_image(0,0),
-            'map_6' : self.get_image(259,0),
-            'map_7' : self.get_image(518,0),
-            'map_8' : self.get_image(774,0),
-            'map_9' : self.get_image(0,0),
-            'map_10' : self.get_image(259,0),
-            'map_11' : self.get_image(518,0)
+            'map_1': self.get_image(1, 1),
+            'map_2': self.get_image(259, 1),
+            'map_3': self.get_image(516, 1),
+            'map_4': self.get_image(1, 147),
+            'map_5': self.get_image(259, 147),
+            'map_6': self.get_image(516, 147),
+            'map_7': self.get_image(1, 293),
+            'map_8': self.get_image(259, 293),
+            'map_9': self.get_image(516, 293),
+            'map_10': self.get_image(1, 438),
+            'map_11': self.get_image(259, 438)
         }
-        # Position du sprite dans la map
-        self.position = [x, y]
 
-        # Liste des maps
-        self.map = ['map_1','map_2','map_3','map_4','map_5','map_6','map_7','map_8','map_9','map_10','map_11']
+        self.map_list = [f'map_{k}' for k in range(1, 12)]
+        self.random_map(random.choice(self.map_list))
 
-        # Choisi une vignette au hasard au début du jeu
-        self.current_map = random.choice(self.map)
-        self.change_map(self.current_map)
-
-    def change_map(self, name):
+    def random_map(self, name):
         self.image = self.images[name]
     
     def get_image(self, x, y):
-        full_screen = pg.Surface([800, 600])
-        sprite_image = pg.Surface([258, 146])
-        # Extrait une vignette du sprite_sheet
-        sprite_image.blit(self.sprite_sheet, (0, 0), (x, y, 258, 146))
-        # Agrandi la vignette
-        pg.transform.scale(sprite_image, (800, 600), full_screen)
-        # Retourne la vignette en 800 x 600 px
+        full_screen = pg.Surface([DSP_WIDTH, DSP_HEIGHT])
+        sprite_image = pg.Surface(self.img_size)
+        sprite_image.blit(self.sprite_sheet, (0, 0), (x, y, self.img_width, self.img_height))
+        pg.transform.scale(sprite_image, (DSP_WIDTH, DSP_HEIGHT), full_screen)
         return full_screen
-
-map = Map(0, 0)
-
-while run:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-
-    pg.display.flip()
-    fond = SCREEN.get_rect()
-    SCREEN.blit(map.image, fond)
-    pg.display.update()
-
-
-
-
