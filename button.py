c6540c0355c518d56ae06b4121839ada8c1a910d
bruno-coulet <bruno.coulet@laplateforme.io>
2024-01-16 17:34:49 +0100

    contour des boutons et  ajout de polices

--- button.py
+++ button.py
@@ -30,7 +30,9 @@ DODGE_BTN_Y = 525
 CHANGE_BTN_X = 590
 CHANGE_BTN_Y = 525
 
-
+KANIT = 'assets/fonts/kanit_medium.ttf'
+KANIT_BOLD = 'assets/fonts/kanit_extraboldItalic.ttf'
+MARHEY = 'assets/fonts/marhey.ttf'
 
 
 
@@ -39,7 +41,9 @@ class Button:
     def __init__(self, btn_text, x, y, width, height, attribute ):
         pg.font.init()
         # self.FONT = pg.font.Font(None, 25)
-        self.FONT = pg.font.SysFont('arial', 25)
+        # self.FONT = pg.font.SysFont('arial', 25)
+        # self.FONT = pg.font.Font(KANIT, 20)
+        self.FONT = pg.font.Font(MARHEY, 18)
         self.btn_text = btn_text
         self.x = x
         self.y = y
@@ -48,6 +52,8 @@ class Button:
         self.attribute = attribute
 
     def draw(self, screen):
+        pg.draw.rect(screen, COLORS['ORANGE'], (self.x-2, self.y-2, self.width, self.height), border_radius=20)
+        pg.draw.rect(screen, COLORS['YELLOW'], (self.x, self.y, self.width+4, self.height+4), border_radius=20)
         pg.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), border_radius=20)
         text_surface = self.FONT.render(self.btn_text, True, (255, 255, 255))
         text_rect = text_surface.get_rect(center=(self.x + self.width // 2, self.y + self.height // 2))
@@ -57,18 +63,6 @@ class BattleButton(Button):
     def __init__(self, color, btn_text, x, y, width, height, attribute):
         super().__init__(btn_text, x, y, width, height, attribute)
         self.color = color
-
-
-    # ATTACK_BUTTON = pg.Rect(ATTACK_BTN_X,ATTACK_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # SPECIAL_BUTTON = pg.Rect(SPECIAL_BTN_X,SPECIAL_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # DODGE_BUTTON = pg.Rect(DODGE_BTN_X,DODGE_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # CHANGE_BUTTON = pg.Rect(CHANGE_BTN_X,CHANGE_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # def draw_btn(self):
-    #     pg.draw.rect(SCREEN, 'RED', ATTACK_BUTTON)
-    #     pg.draw.rect(SCREEN, 'RED', SPECIAL_BUTTON)
-    #     pg.draw.rect(SCREEN, 'RED', DODGE_BUTTON)
-    #     pg.draw.rect(SCREEN, 'RED', CHANGE_BUTTON)
-
 
 if __name__ == "__main__":
     pg.init()
@@ -81,9 +75,9 @@ if __name__ == "__main__":
     map1 = Map()
 
 
-    attack_btn = BattleButton(btn_text = "attaque", color = COLORS['RED'], x = ATTACK_BTN_X, y = ATTACK_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = 'atk_method')
-    special_btn = BattleButton(btn_text = "attaque spéciale", color = COLORS['DARK_RED'], x = SPECIAL_BTN_X, y = SPECIAL_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = 'spe_atk')
-    dodge_btn = BattleButton(btn_text = "esquive", color = COLORS['BLUE'], x = DODGE_BTN_X, y = DODGE_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = '___')
+    attack_btn = BattleButton(btn_text = "Attaque", color = COLORS['RED'], x = ATTACK_BTN_X, y = ATTACK_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = 'atk_method')
+    special_btn = BattleButton(btn_text = "Attaque spéciale", color = COLORS['DARK_RED'], x = SPECIAL_BTN_X, y = SPECIAL_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = 'spe_atk')
+    dodge_btn = BattleButton(btn_text = "Courage, fuyons !", color = COLORS['LIGHT_BLUE'], x = DODGE_BTN_X, y = DODGE_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = '___')
     change_btn = BattleButton(btn_text = "Changer de pokémon", color = COLORS['DARK_BLUE'], x = CHANGE_BTN_X, y = CHANGE_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = '___')
 
     run = True
@@ -107,7 +101,19 @@ if __name__ == "__main__":
     pg.quit()
 
         
-            
+
+
+    # ATTACK_BUTTON = pg.Rect(ATTACK_BTN_X,ATTACK_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
+    # SPECIAL_BUTTON = pg.Rect(SPECIAL_BTN_X,SPECIAL_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
+    # DODGE_BUTTON = pg.Rect(DODGE_BTN_X,DODGE_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
+    # CHANGE_BUTTON = pg.Rect(CHANGE_BTN_X,CHANGE_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
+    # def draw_btn(self):
+    #     pg.draw.rect(SCREEN, 'RED', ATTACK_BUTTON)
+    #     pg.draw.rect(SCREEN, 'RED', SPECIAL_BUTTON)
+    #     pg.draw.rect(SCREEN, 'RED', DODGE_BUTTON)
+    #     pg.draw.rect(SCREEN, 'RED', CHANGE_BUTTON)
+
+
         # Draw the button
         # button.draw_attack_btn(SCREEN)
         # button.draw_spe_attack_btn(SCREEN)
