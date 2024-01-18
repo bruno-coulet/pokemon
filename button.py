0fde9ce9783f9265f2bd9b7e5f11212742a018a0
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-01-18 16:19:24 +0100

    There is a lot of work on this button class to do.

--- button.py
+++ button.py
@@ -74,7 +74,6 @@ if __name__ == "__main__":
     battle = GuiBattle(pok1, pok2, dex)
     map1 = Map()
 
-
     attack_btn = BattleButton(btn_text = "Attaque", color = COLORS['RED'], x = ATTACK_BTN_X, y = ATTACK_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = 'atk_method')
     special_btn = BattleButton(btn_text = "Attaque spéciale", color = COLORS['DARK_RED'], x = SPECIAL_BTN_X, y = SPECIAL_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = 'spe_atk')
     dodge_btn = BattleButton(btn_text = "Courage, fuyons !", color = COLORS['LIGHT_BLUE'], x = DODGE_BTN_X, y = DODGE_BTN_Y, width = BATTLE_BTN_WIDTH, height = BATTLE_BTN_HEIGHT, attribute = '___')
@@ -95,63 +94,6 @@ if __name__ == "__main__":
         dodge_btn.draw(SCREEN)
         change_btn.draw(SCREEN)
 
-
         pg.display.flip()
 
     pg.quit()
-
-        
-
-
-    # ATTACK_BUTTON = pg.Rect(ATTACK_BTN_X,ATTACK_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # SPECIAL_BUTTON = pg.Rect(SPECIAL_BTN_X,SPECIAL_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # DODGE_BUTTON = pg.Rect(DODGE_BTN_X,DODGE_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # CHANGE_BUTTON = pg.Rect(CHANGE_BTN_X,CHANGE_BTN_Y,BATTLE_BTN_WIDTH,BATTLE_BTN_HEIGHT)
-    # def draw_btn(self):
-    #     pg.draw.rect(SCREEN, 'RED', ATTACK_BUTTON)
-    #     pg.draw.rect(SCREEN, 'RED', SPECIAL_BUTTON)
-    #     pg.draw.rect(SCREEN, 'RED', DODGE_BUTTON)
-    #     pg.draw.rect(SCREEN, 'RED', CHANGE_BUTTON)
-
-
-        # Draw the button
-        # button.draw_attack_btn(SCREEN)
-        # button.draw_spe_attack_btn(SCREEN)
-        # button.draw_dodge_btn(SCREEN)
-        # button.draw_change_pok_btn(SCREEN)
-
-
-        # Draw the special button
-        # special_button.draw()
-        
-
-
-    # def draw_text(self, text, CREME, SCREEN, x, y):
-    #     textobj = self.FONT.render(text, 1, COLORS['CREME'])
-    #     textrect = textobj.get_rect()
-    #     textrect.topleft = (0, 0)
-    #     SCREEN.blit(textobj, textrect) 
-
-    # def draw_attack_btn(self, screen):
-    #     pg.draw.rect(screen, 'RED', (380, 450, BTN_WIDTH, BTN_HEIGHT),border_radius=20)
-    #     """" Create a font object,
-    #         Create a text surface
-    #         Get the text rectangle
-    #         Center the text on the button
-    #         Blit the text onto the screen
-    #     """
-    #     font = pg.font.Font(None, 36)  
-    #     text_surface = font.render("Attack", True, (255, 255, 255)) 
-    #     text_rect = text_surface.get_rect()
-    #     text_rect.center = (380+BTN_WIDTH // 2, 450+BTN_HEIGHT // 2)
-    #     screen.blit(text_surface, text_rect)
-        
-
-    # def draw_spe_attack_btn(self, screen):
-    #     pg.draw.rect(screen, 'CYAN', (SPECIAL_BTN_X, SPECIAL_BTN_Y, BTN_WIDTH, BTN_HEIGHT))
-        
-    # def draw_dodge_btn(self, screen):
-    #     pg.draw.rect(screen, 'GREY', (380, 525, BTN_WIDTH, BTN_HEIGHT))
-
-    # def draw_change_pok_btn(self, screen):
-    #     pg.draw.rect(screen, 'GREY', (590, 525, BTN_WIDTH, BTN_HEIGHT))
