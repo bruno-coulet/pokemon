66633fd3ca07af5eebe9238897346f6913824acb
bruno-coulet <bruno.coulet@laplateforme.io>
2024-01-22 09:59:20 +0100

    nouvelle branche 'welcome'

--- game.py
+++ game.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@authors: Cyril GENISSON
+@authors: Cyril GENISSON, Bruno COULET
 @file: game.py
 
 @project: Pokémon
 @licence: GPLv3
 """
+
+"""faire la page d'accueil du jeux dans le fichier Game.py avec 5 boutons:
+-  Nouvelle partie
+- Continuer
+- Pokédex
+- Générique
+- Crédits"""
 
 
 class Game:
