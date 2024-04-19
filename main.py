06a0b3f643ed2c57f4abbb8acd8d61056de41ad5
Bruno Coulet <147488292+bruno-coulet@users.noreply.github.com>
2024-04-19 13:31:00 +0200

    Update main comments.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
                     f'{SP_POK_PATH}{id_pok}-regular.png'):
                 urllib.request.urlretrieve(json_data[id_pok]['sprites']['regular'],
                                            f'{SP_POK_PATH}{id_pok}-regular.png')
-                # wget.download(json_data[id_pok]['sprites']['regular'], out=f'{SP_POK_PATH}{id_pok}-regular.png')
+
             if json_data[id_pok]['sprites']['shiny'] is not None and not os.path.exists(
                     f'{SP_POK_PATH}{id_pok}-shiny.png'):
                 urllib.request.urlretrieve(json_data[id_pok]['sprites']['shiny'],
